d340b9ed5817962757919cabdc2fc7a60a90c24a
christina <>
2021-07-27 14:53:57 -0500

    edit filter_list

--- filter_list.py
+++ filter_list.py
@@ -1,21 +1,17 @@
 '''Create a function that takes a list of non-negative integers and strings and 
 returns a new list with the strings filtered out.'''
 
+
 def filter_list(lst):
     '''Returns new list with strings filtered out'''
-    new_list
+    new_list = []
+    for item in lst:
+        if type(item) == int:
+            new_list.append(item)
+    
+    return new_list
 
 
-
-
-
-
-
-
-
-'''Example
-
-filter_list([1,2,'a','b']) == [1,2]
-filter_list([1,'a','b',0,15]) == [1,0,15]
-filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
-'''
+print(filter_list([1, 2, 'a', 'b']))  # [1,2]
+print(filter_list([1, 'a', 'b', 0, 15]))  # [1,0,15]
+print(filter_list([1, 2, 'aasf', '1', '123', 123]))  # [1,2,123]
