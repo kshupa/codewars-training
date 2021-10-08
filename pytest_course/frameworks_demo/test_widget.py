869641ed3eb43a096d8aec9e3c6e5b16fbfcd5b7
christina <>
2021-10-08 15:14:32 -0500

    add pytest.ini

--- pytest_course/frameworks_demo/test_widget.py
+++ pytest_course/frameworks_demo/test_widget.py
@@ -1,3 +1,10 @@
 def test_widget_functions_as_expected():
     assert True
-    
+
+
+def test_widget_fails():
+    assert False
+
+
+def test_convenience_function():
+    print("I do some extra work")
